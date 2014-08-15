74c6412bff8a886bbe1f6e5157246b7d1cf87da3
Hasta Ragil <npcmamaz@gmail.com>
2014-08-16 01:10:02 +0700

    Add migration file for photos table

---
+++ app/database/migrations/2014_08_15_180724_create_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('product_id')->unsigned();
+			$table->timestamps();
+			$table->foreign('product_id')->references('id')->on('products');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photos');
+	}
+
+}
