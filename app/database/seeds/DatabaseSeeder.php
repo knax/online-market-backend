9799e1d1aefdf8a3e47d6b0fb971e63d7cfeba27
Hasta Ragil <npcmamaz@gmail.com>
2014-10-06 08:58:54 +0700

    Change order of seeding

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -3,22 +3,21 @@
 class DatabaseSeeder extends Seeder {
 
 	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
+	* Run the database seeds.
+	*
+	* @return void
+	*/
 	public function run()
 	{
 		Eloquent::unguard();
-
-		$this->call('CategoryTableSeeder');
-		$this->call('ProductTableSeeder');
-		$this->call('PhotoTableSeeder');
 		$this->call('AccessListTableSeeder');
 		$this->call('UserTypeTableSeeder');
 		$this->call('UserTypeAccessListTableSeeder');
 		$this->call('UserTableSeeder');
 		$this->call('ContactTableSeeder');
+		$this->call('CategoryTableSeeder');
+		$this->call('ProductTableSeeder');
+		$this->call('PhotoTableSeeder');
 	}
 
 }
