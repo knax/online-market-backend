9f6c391add1ffae5e3f0a826acc00dd3614bcee0
Hasta Ragil <npcmamaz@gmail.com>
2014-10-06 09:04:13 +0700

    Add route

--- app/routes.php
+++ app/routes.php
@@ -10,3 +10,21 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
+Route::get('test', 'TestController@test');
+
+Route::group(array('prefix' => 'api/v1'), function(){
+
+    Route::group(array('prefix' => 'product'), function(){
+        Route::get('sort_by/{type}', 'ProductsListController@sortBy');
+        Route::get('/', 'ProductsListController@getAll');
+        Route::get('all', 'ProductsListController@getAll');
+    });
+
+    Route::group(array('prefix' => 'contact'), function(){
+        Route::get('/', 'ContactController@getAll');
+        Route::get('all', 'ContactController@getAll');
+        Route::get('type/{type}', 'ContactController@getByType');
+    });
+});
+Route::get('test', 'ProductsListController@test');
